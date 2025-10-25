079f2320d0b94acd200e192177d023b1779b866b
sarahb-91 <sarah.bordes91@gmail.com>
2025-10-25 19:10:21 +0000

    feat: print _putchar using custom function

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char word[] = "_putchar";
+	int i;
+
+	for (i = 0; word[i] != '\0'; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
