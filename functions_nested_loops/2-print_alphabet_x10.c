3846d5961f1f85d1915e976132725fd11d1d37f6
sarahb-91 <sarah.bordes91@gmail.com>
2025-10-25 19:58:25 +0000

    feat: add function to print lowercase alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		print_alphabet();
+	}
+}
