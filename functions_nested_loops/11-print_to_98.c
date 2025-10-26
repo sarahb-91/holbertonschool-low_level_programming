fdc0eec12945fbe057a8adb6860255e54a3c0880
sarahb-91 <sarah.bordes91@gmail.com>
2025-10-26 22:16:03 +0000

    fix: corrected indentation and style issues

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,24 +9,24 @@
  */
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        for (; n <= 98; n++)
-        {
-            if (n != 98)
-                printf("%d, ", n);
-            else
-                printf("%d\n", n);
-        }
-    }
-    else
-    {
-        for (; n >= 98; n--)
-        {
-            if (n != 98)
-                printf("%d, ", n);
-            else
-                printf("%d\n", n);
-        }
-    }
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d\n", n);
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d\n", n);
+		}
+	}
 }
